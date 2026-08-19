1c1f7cd1bbc4ffaa8de4209782a6208727bb847d
Raikrithika <krithikarai091@gmail.com>
2026-08-19 19:11:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMode() {
+    document.body.classList.toggle("dark-mode");
+}
+
+function showMessage() {
+    document.getElementById("message").innerHTML =
+        "Thank you for visiting my portfolio! 😊";
+}
